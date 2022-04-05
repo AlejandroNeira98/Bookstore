40ed9163e033e43cd21d2dba68e2b3922f0c520d
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-04-04 19:40:23 -0500

    Add actions, reducers and store

---
+++ src/redux/books/books.js
@@ -0,0 +1,31 @@
+const ADD = 'bookstore/books/ADD';
+const REMOVE = 'bookstore/books/REMOVE';
+
+export default function reducer(state = [], action) {
+  switch (action.type){
+    case ADD:
+      return [...state, action.book];
+    case REMOVE:
+      return state.map((book) => {
+        if (book.id !== action.id){
+          return book;
+        }
+      })
+    default: 
+      return state;
+  }
+}
+
+export function addBook(book){
+  return {
+    type: ADD,
+    book,
+  }
+}
+
+export function removeBook(id){
+  return {
+    type: REMOVE,
+    id,
+  }
+}
