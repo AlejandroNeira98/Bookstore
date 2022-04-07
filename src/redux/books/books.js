889bbeaae3de644b3911b47e831bb6f6be0da650
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-04-07 13:53:41 -0500

    Linters

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,20 +1,20 @@
-import { url } from '../configureStore';
+export const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/cztRrb2hdveZJBbq0L1F/';
 
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 const FETCH_BOOKLIST = 'bookstore/books/FETCH_BOOKLIST';
 
 export default function reducer(state = [], action) {
+  const bookList = [];
   switch (action.type) {
     case ADD:
       return [...state, action.book];
     case REMOVE:
       return state.filter((book) => book.item_id !== action.item_id);
     case FETCH_BOOKLIST:
-      const bookList = [];
-      for (let item in action.data){
-        bookList.push(action.data[item][0])
-      }
+      Object.values(action.data).forEach((item) => {
+        bookList.push(item[0]);
+      });
       return bookList;
     default:
       return state;
@@ -23,29 +23,29 @@ export default function reducer(state = [], action) {
 
 export function addBook(book) {
   return async (dispatch) => {
-    await fetch(`${url}books`,{
+    await fetch(`${url}books`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
       body: JSON.stringify(book),
-    }).then(() => dispatch({ type: ADD, book}))
+    }).then(() => dispatch({ type: ADD, book }));
   };
 }
 
-export function removeBook(item_id) {
+export function removeBook(id) {
   return async (dispatch) => {
-    await fetch(`${url}books/${item_id}`,{
-      method: 'DELETE'
-    }).then(() => dispatch({type: REMOVE, item_id}))
+    await fetch(`${url}books/${id}`, {
+      method: 'DELETE',
+    }).then(() => dispatch({ type: REMOVE, item_id: id }));
   };
 }
 
-export function getBookList () { 
+export function getBookList() {
   return async (dispatch) => {
-    await fetch (`${url}books`)
+    await fetch(`${url}books`)
       .then((response) => response.json())
       .then((data) => dispatch({ type: FETCH_BOOKLIST, data }));
-  }
+  };
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,6 @@ import thunk from 'redux-thunk';
 import books from './books/books';
 import categories from './categories/categories';
 
-export const url ='https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/cztRrb2hdveZJBbq0L1F/';
-
 const rootReducer = combineReducers({
   books,
   categories,
